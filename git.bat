42a32f60b09fd7220c35e45ff77ed614ac24c1d9
Tanujit Roy <30634933+tanujit@users.noreply.github.com>
2018-07-15 22:22:53 +0530

    Create git.bat

---
+++ git.bat
@@ -0,0 +1,23 @@
+@echo off
+color a
+echo                 `+dNNh-                                                  `ymMMmy`                  
+echo                `+mMMMMMMh:                                                oMMMMMMs       .--.       
+echo                :mMMMMMMMMMd/`                                             -NMMMMN:      `NMMN`      
+echo            .oh/` /dMMMMMMMMMd/`                                            `://:`      `yMMMN`      
+echo          .sNMMMd/` :++yNMMMMMMm+`                      ```...``````````  `.......`  .-+dMMMMN/::::: 
+echo        -yNMMMMMMMh     .NMMMMMMMm+`                 `/ydmNNNNNmmmmmmmmd  dNNNNNNNo .MMMMMMMMMMMMMMm 
+echo      -yNMMMMMMMMMN-     yNMMMMMMMMmo.              /mMMMMNmddmNMMMMNddh  shNMMMMMs `yyyMMMMMMyyyyys 
+echo    :hNMMMMMMMMMMMMNo  -. -yNMMMMMMMMmo.           :MMMMMy-`  `:mMMMN:      oMMMMMs    `NMMMMN`      
+echo `:hMMMMMMMMMMMMMMMMh  ymo. -ssydMMMMMMNs.         oMMMMM.      oMMMMh      oMMMMMs    `NMMMMN`      
+echo yMMMMMMMMMMMMMMMMMMh  yMMN/    `oMMMMMMMN+        :MMMMMy-```./mMMMMy      oMMMMMs    `NMMMMN`      
+echo yMMMMMMMMMMMMMMMMMMh  yMMMy     /MMMMMMMN+         +NMMMMNmddmMMMMMh.      oMMMMMs    `NMMMMN`      
+echo  :hNMMMMMMMMMMMMMMMh  yMMMMho/+yNMMMMMNy-           -hMMMNNMMNNmds:`       oMMMMMs    `NMMMMN`      
+echo    -yNMMMMMMMMMMMMMy  sMMMMMMMMMMMMMNs.           `+mMMMs-----.``          oMMMMMs     NMMMMMy/::/+`
+echo      -yNMMMMMMMMMN/`  `/MMMMMMMMMMNs.             sMMMMMNmmmmdddhys+-   `yhNMMMMMNdh.  +NMMMMMMMMMMs
+echo        .sNMMMMMMMm`    `NMMMMMMMmo.               /mMMMMMMMMMMMMMMMMNd. `ddddddddddd-   -ohdmmddhs+-
+echo          .sNMMMMMMh/-:+dMMMMMMm+`                 -dMMMysyyhhhddNMMMMMy  ```````````       `````    
+echo            .oNMMMMMMMMMMMMMMm+`                  `NMMMy        `+MMMMMs                             
+echo              `+mMMMMMMMMMMm/`                    `NMMMMhso+++oshNMMMMh`                             
+echo                `+mMMMMMMd/`                       .smMMMMMMMMMMMMMds:                               
+echo                   /dMNh:                             `.-://///:-.`      `   
+pause >nul
